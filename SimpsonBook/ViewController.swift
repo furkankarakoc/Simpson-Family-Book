91a3da2326db3f0d5ea606243ad9a6c3603c4eab
furkankarakocmobilist <furkan.karakoc@mobilistbilisim.com>
2023-01-21 17:57:56 +0300

    Initial Commit

---
+++ SimpsonBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpsonBook
+//
+//  Created by Furkan Karakoc on 21.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
